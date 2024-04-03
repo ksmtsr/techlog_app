bb536e787ed08b9d6b858f05359306c5a206b068
s-kishimoto <sarii.4u@icloud.com>
2024-04-03 10:01:51 +0900

    deviseインストールと初期設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        # 新規登録時に nickname というキーのパラメータも許可することを設定
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
 end
